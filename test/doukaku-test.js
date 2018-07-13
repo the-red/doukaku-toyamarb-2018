93a5a37475a726ffaff3f2890e783ba2aa116846
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 00:45:34 +0900

    feat: ポーカー 回答

--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -1,29 +1,77 @@
 require('./helper')
 
-it('hoge抽出', () => {
-  assert.deepStrictEqual(doukaku.pluckHoge('123'), ['1', '2', '3'])
+it('カードを認識', () => {
+  assert.deepStrictEqual(doukaku.pluckCards('D3C3C10D10S3'), [
+    { number: '3', suit: 'D' },
+    { number: '3', suit: 'C' },
+    { number: '10', suit: 'C' },
+    { number: '10', suit: 'D' },
+    { number: '3', suit: 'S' },
+  ])
+  assert.deepStrictEqual(doukaku.pluckCards('DACAC10D10S3'), [
+    { number: 'A', suit: 'D' },
+    { number: 'A', suit: 'C' },
+    { number: '10', suit: 'C' },
+    { number: '10', suit: 'D' },
+    { number: '3', suit: 'S' },
+  ])
 })
-it('fuga判定', () => {
-  assert.deepStrictEqual(doukaku.isFuga('1'), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('123')), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('234')), false)
-})
-it('piyo判定', () => {
-  assert.deepStrictEqual(doukaku.isPiyo('2'), true)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('123')), false)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('234')), true)
-})
-it('hoge fuga piyo判定', () => {
+it('フォーカード判定', () => {
   assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('345')),
-    'hoge'
+    doukaku.is4K(doukaku.pluckCards('D3C3C10D10S3')),
+    false
+  )
+  assert.deepStrictEqual(doukaku.is4K(doukaku.pluckCards('D3C3C10D3S3')), true)
+})
+it('フルハウス判定', () => {
+  assert.deepStrictEqual(doukaku.isFH(doukaku.pluckCards('D3C3C10D10S3')), true)
+  assert.deepStrictEqual(doukaku.isFH(doukaku.pluckCards('D3C3C10D3S3')), false)
+})
+it('スリーカード判定', () => {
+  assert.deepStrictEqual(doukaku.is3K(doukaku.pluckCards('D3C3C10D9S3')), true)
+  assert.deepStrictEqual(
+    doukaku.is3K(doukaku.pluckCards('D3C3C10D10S3')),
+    false
+  )
+})
+it('ツーペア判定', () => {
+  assert.deepStrictEqual(doukaku.is2P(doukaku.pluckCards('D3C3C10D10S2')), true)
+  assert.deepStrictEqual(
+    doukaku.is2P(doukaku.pluckCards('D3C3C10D10S3')),
+    false
+  )
+})
+it('ワンペア判定', () => {
+  assert.deepStrictEqual(doukaku.is1P(doukaku.pluckCards('D3C3C10D9S2')), true)
+  assert.deepStrictEqual(doukaku.is1P(doukaku.pluckCards('DACAC10D9S2')), true)
+  assert.deepStrictEqual(
+    doukaku.is1P(doukaku.pluckCards('D3C3C10D10S2')),
+    false
+  )
+})
+it('ロール判定', () => {
+  assert.deepStrictEqual(
+    doukaku.judgeRole(doukaku.pluckCards('D3C3C10D3S3')),
+    '4K'
   )
   assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('123')),
-    'fuga'
+    doukaku.judgeRole(doukaku.pluckCards('D3C3C10D10S3')),
+    'FH'
   )
   assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('234')),
-    'piyo'
+    doukaku.judgeRole(doukaku.pluckCards('D3C3C10D9S3')),
+    '3K'
+  )
+  assert.deepStrictEqual(
+    doukaku.judgeRole(doukaku.pluckCards('D3C3C10D10S2')),
+    '2P'
+  )
+  assert.deepStrictEqual(
+    doukaku.judgeRole(doukaku.pluckCards('DACAC10D9S2')),
+    '1P'
+  )
+  assert.deepStrictEqual(
+    doukaku.judgeRole(doukaku.pluckCards('DACKC10D9S2')),
+    '--'
   )
 })
