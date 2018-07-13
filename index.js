cf622f724f22c7a7d6ee3f327831dc776eadc24f
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 00:20:46 +0900

    feat: 関数4つをテストするサンプル作成

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 
 const doukaku = require('./lib/doukaku')
 const argv = process.argv.slice(2)
-const result = doukaku(...argv)
+const result = doukaku.main(...argv)
 console.log(result)
--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -1,7 +1,29 @@
 require('./helper')
 
-it('第一引数をそのまま返す', () => {
-  assert.strictEqual(doukaku('1', '2'), '1')
-  assert.notStrictEqual(doukaku('1', '2'), 1)
-  assert.notStrictEqual(doukaku('1', '2'), '2')
+it('hoge抽出', () => {
+  assert.deepStrictEqual(doukaku.pluckHoge('123'), ['1', '2', '3'])
+})
+it('fuga判定', () => {
+  assert.deepStrictEqual(doukaku.isFuga('1'), true)
+  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('123')), true)
+  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('234')), false)
+})
+it('piyo判定', () => {
+  assert.deepStrictEqual(doukaku.isPiyo('2'), true)
+  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('123')), false)
+  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('234')), true)
+})
+it('hoge fuga piyo判定', () => {
+  assert.deepStrictEqual(
+    doukaku.judgeFugaPiyo(doukaku.pluckHoge('345')),
+    'hoge'
+  )
+  assert.deepStrictEqual(
+    doukaku.judgeFugaPiyo(doukaku.pluckHoge('123')),
+    'fuga'
+  )
+  assert.deepStrictEqual(
+    doukaku.judgeFugaPiyo(doukaku.pluckHoge('234')),
+    'piyo'
+  )
 })
