bc0a9582fa27fdff374807eb2e093fe1b58f7208
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 15:58:48 +0900

    refactor: forループに変更

--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -18,3 +18,41 @@ it('水の合計算出', () => {
   blocks[3].water = 0
   assert.deepStrictEqual(doukaku.sumWater(blocks), 3)
 })
+
+it('エッジブロックの判定', () => {
+  const blocks = [
+    new doukaku.Block(3),
+    new doukaku.Block(1),
+    new doukaku.Block(2),
+    new doukaku.Block(4),
+  ]
+  doukaku.setEdges(blocks)
+  assert.deepStrictEqual(blocks[0].isEdge, true)
+  assert.deepStrictEqual(blocks[1].isEdge, false)
+  assert.deepStrictEqual(blocks[2].isEdge, false)
+  assert.deepStrictEqual(blocks[3].isEdge, true)
+})
+
+it('エッジブロックの判定 途中に0がある場合', () => {
+  const blocks = [
+    new doukaku.Block(9),
+    new doukaku.Block(0),
+    new doukaku.Block(3),
+    new doukaku.Block(1),
+    new doukaku.Block(1),
+    new doukaku.Block(1),
+    new doukaku.Block(1),
+    new doukaku.Block(2),
+    new doukaku.Block(8),
+  ]
+  doukaku.setEdges(blocks)
+  assert.deepStrictEqual(blocks[0].isEdge, true)
+  assert.deepStrictEqual(blocks[1].isEdge, true)
+  assert.deepStrictEqual(blocks[2].isEdge, true)
+  assert.deepStrictEqual(blocks[3].isEdge, false)
+  assert.deepStrictEqual(blocks[4].isEdge, false)
+  assert.deepStrictEqual(blocks[5].isEdge, false)
+  assert.deepStrictEqual(blocks[6].isEdge, false)
+  assert.deepStrictEqual(blocks[7].isEdge, false)
+  assert.deepStrictEqual(blocks[8].isEdge, true)
+})
