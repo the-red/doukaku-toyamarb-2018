748bfd5869c36c98304bc41930c1dd4a10d0ea9b
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 15:47:30 +0900

    feat: 水の合計算出

--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -4,3 +4,17 @@ it('ブロックインスタンスの作成', () => {
   const block = new doukaku.Block(5)
   assert.deepStrictEqual(block.height, 5)
 })
+
+it('水の合計算出', () => {
+  const blocks = [
+    new doukaku.Block(3),
+    new doukaku.Block(1),
+    new doukaku.Block(2),
+    new doukaku.Block(4),
+  ]
+  blocks[0].water = 0
+  blocks[1].water = 2
+  blocks[2].water = 1
+  blocks[3].water = 0
+  assert.deepStrictEqual(doukaku.sumWater(blocks), 3)
+})
