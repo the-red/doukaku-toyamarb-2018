dd96ee6e6b7000e46f9d75a965e61a42c6c146ed
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 15:41:30 +0900

    feat: ブロックオブジェクト作成

--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -1,29 +1,6 @@
 require('./helper')
 
-it('hoge抽出', () => {
-  assert.deepStrictEqual(doukaku.pluckHoge('123'), ['1', '2', '3'])
-})
-it('fuga判定', () => {
-  assert.deepStrictEqual(doukaku.isFuga('1'), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('123')), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('234')), false)
-})
-it('piyo判定', () => {
-  assert.deepStrictEqual(doukaku.isPiyo('2'), true)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('123')), false)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('234')), true)
-})
-it('hoge fuga piyo判定', () => {
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('345')),
-    'hoge'
-  )
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('123')),
-    'fuga'
-  )
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('234')),
-    'piyo'
-  )
+it('ブロックインスタンスの作成', () => {
+  const block = new doukaku.Block(5)
+  assert.deepStrictEqual(block.height, 5)
 })
