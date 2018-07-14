f89d80a108c18591dd119cd5f68b8b579777bfb5
Hisaki Akaza <4494300+TheRed@users.noreply.github.com>
2018-07-14 13:32:28 +0900

    feat: レジオブジェクト作成

--- test/doukaku-test.js
+++ test/doukaku-test.js
@@ -1,29 +1,51 @@
 require('./helper')
 
-it('hoge抽出', () => {
-  assert.deepStrictEqual(doukaku.pluckHoge('123'), ['1', '2', '3'])
+it('レジのインスタンスを作成', () => {
+  const register = new doukaku.Register(1, 2)
+  assert.deepStrictEqual(register.id, 1)
+  assert.deepStrictEqual(register.spec, 2)
+  assert.deepStrictEqual(register.queue, 0)
 })
-it('fuga判定', () => {
-  assert.deepStrictEqual(doukaku.isFuga('1'), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('123')), true)
-  assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('234')), false)
+it('レジに人が並ぶ', () => {
+  const register = new doukaku.Register(1, 2)
+  register.push(4)
+  assert.deepStrictEqual(register.queue, 4)
 })
-it('piyo判定', () => {
-  assert.deepStrictEqual(doukaku.isPiyo('2'), true)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('123')), false)
-  assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('234')), true)
+it('レジが会計をする', () => {
+  const register1 = new doukaku.Register(1, 2)
+  register1.push(4)
+  register1.pop()
+  assert.deepStrictEqual(register1.queue, 2)
+
+  const register2 = new doukaku.Register(2, 7)
+  register2.push(4)
+  register2.pop()
+  assert.deepStrictEqual(register2.queue, 0)
 })
-it('hoge fuga piyo判定', () => {
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('345')),
-    'hoge'
-  )
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('123')),
-    'fuga'
-  )
-  assert.deepStrictEqual(
-    doukaku.judgeFugaPiyo(doukaku.pluckHoge('234')),
-    'piyo'
-  )
-})
+// it('hoge抽出', () => {
+//   assert.deepStrictEqual(doukaku.pluckHoge('123'), ['1', '2', '3'])
+// })
+// it('fuga判定', () => {
+//   assert.deepStrictEqual(doukaku.isFuga('1'), true)
+//   assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('123')), true)
+//   assert.deepStrictEqual(doukaku.isFuga(doukaku.pluckHoge('234')), false)
+// })
+// it('piyo判定', () => {
+//   assert.deepStrictEqual(doukaku.isPiyo('2'), true)
+//   assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('123')), false)
+//   assert.deepStrictEqual(doukaku.isPiyo(doukaku.pluckHoge('234')), true)
+// })
+// it('hoge fuga piyo判定', () => {
+//   assert.deepStrictEqual(
+//     doukaku.judgeFugaPiyo(doukaku.pluckHoge('345')),
+//     'hoge'
+//   )
+//   assert.deepStrictEqual(
+//     doukaku.judgeFugaPiyo(doukaku.pluckHoge('123')),
+//     'fuga'
+//   )
+//   assert.deepStrictEqual(
+//     doukaku.judgeFugaPiyo(doukaku.pluckHoge('234')),
+//     'piyo'
+//   )
+// })
